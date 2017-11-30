62561a3671693b7b0c56715903274c63d1b7a0e5
Moritz Schilling <moritz.schilling@dfki.de>
2017-11-30 17:56:18 +0100

    Software graph class ported and cleansed
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

---
+++ include/SoftwareGraph.hpp
@@ -0,0 +1,113 @@
+#ifndef _SOFTWARE_GRAPH_HPP
+#define _SOFTWARE_GRAPH_HPP
+
+#include "ComponentNetwork.hpp"
+
+namespace Software {
+
+/*
+    Some notes:
+    The underlying fundament is the concept graph.
+    In this environment there are two main entities, concepts & relations between them.
+    This class is derived from the concept graph class.
+
+    It introduces these different concepts:
+        ALGORITHM, INPUT, OUTPUT, INTERFACE (for logical specification and skeleton generation)
+        IMPLEMENTATION, DATATYPE (for storing already available implementations of algorithms in different languages)
+    The domain is encoded as follows:
+
+    (ALGORITHM -- has --> INTERFACE)                    optional, inferrable
+    INPUT -- is-a --> INTERFACE
+    OUTPUT -- is-a --> INTERFACE
+    ALGORITHM -- needs(has) --> INPUT
+    ALGORITHM -- provides(has) --> OUTPUT
+    INPUT -- dependsOn(connects) --> OUTPUT
+
+    ALGORITHM <-- realizes(is-a) -- IMPLEMENTATION
+    INTERFACE <-- represents(is-a) -- DATATYPE
+
+    (IMPLEMENTATION -- uses --> DATATYPE)               questionable
+    (IMPLEMENTATION -- dependsOn --> IMPLEMENTATION)    optional, not needed yet
+    (IMPLEMENTATION -- needs --> INPUT)                 redundant
+    (IMPLEMENTATION -- provides --> OUTPUT)             redundant
+
+    If some X is a ALGORITHM then there exists a path of IS-A relations from X to ALGORITHM
+
+    Some example:
+
+        |---------- realizes -- disparity.c
+        v
+    DisparityMap -- needs --> left -- is-a --> Input
+      | |                       |---- is-a --> Image <-- represents -- uint8_t[MAX_X][MAX_Y]
+      | |---------- needs --> right ...
+      |------------ provides --> disparity ...
+
+    NOTE: When merging with the concept of Finite State Machines, this whole thing would become an even whiter box :)
+    NOTE: To support different programming languages, you can create a domains.
+*/
+
+class Graph : public Component::Network
+{
+    public:
+        // Identifiers for the main concepts
+        static const UniqueId AlgorithmId;
+        static const UniqueId InterfaceId;
+        static const UniqueId InputId;
+        static const UniqueId OutputId;
+        static const UniqueId ImplementationId;
+        static const UniqueId DatatypeId;
+
+        // Ids for identifiing main relations
+        static const UniqueId NeedsId;
+        static const UniqueId ProvidesId;
+        static const UniqueId DependsOnId;
+
+        // Constructor/Destructor
+        Graph();
+        Graph(Component::Network& A);
+        ~Graph();
+
+        // Generates the dictionary
+        void createMainConcepts();
+
+        // Factory functions
+        // NOTE: Create classes
+        Hyperedges createAlgorithm(const UniqueId& uid, const std::string& name="Algorithm", const Hyperedges& suids=Hyperedges());
+        Hyperedges createInterface(const UniqueId& uid, const std::string& name="Interface", const Hyperedges& suids=Hyperedges());
+        Hyperedges createInput(const UniqueId& uid, const std::string& name="Input", const Hyperedges& suids=Hyperedges());
+        Hyperedges createOutput(const UniqueId& uid, const std::string& name="Output", const Hyperedges& suids=Hyperedges());
+        Hyperedges createImplementation(const UniqueId& uid, const std::string& name="Implementation", const Hyperedges& suids=Hyperedges());
+        Hyperedges createDatatype(const UniqueId& uid, const std::string& name="DataType", const Hyperedges& suids=Hyperedges());
+
+        // Queries
+        // NOTE: Returns subclasses
+        Hyperedges algorithmClasses(const std::string& name="", const Hyperedges& suids=Hyperedges());
+        Hyperedges interfaceClasses(const std::string& name="", const Hyperedges& suids=Hyperedges());
+        Hyperedges inputClasses(const std::string& name="", const Hyperedges& suids=Hyperedges());
+        Hyperedges outputClasses(const std::string& name="", const Hyperedges& suids=Hyperedges());
+        Hyperedges implementationClasses(const std::string& name="", const Hyperedges& suids=Hyperedges());
+        Hyperedges datatypeClasses(const std::string& name="", const Hyperedges& suids=Hyperedges());
+        // NOTE: Returns instances
+        Hyperedges algorithms(const std::string& name="", const std::string& className="");
+        Hyperedges interfaces(const std::string& name="", const std::string& className="");
+        Hyperedges inputs(const std::string& name="", const std::string& className="");
+        Hyperedges outputs(const std::string& name="", const std::string& className="");
+        Hyperedges implementations(const std::string& name="", const std::string& className="");
+        Hyperedges datatypes(const std::string& name="", const std::string& className="");
+
+        // Facts
+        // NOTE: Only the multidimensionals are used here (more generic)
+        // Algorithms & I/O/P
+        // RULE: A has X -> X is-a Interface
+        // RULE: A provides O -> A has O, O is-a Output (, O is-a Interface)
+        // RULE: A needs I -> A has I, I is-a Input (, I is-a Interface)
+        Hyperedges providesInterface(const Hyperedges& algorithmIds, const Hyperedges& outputIds);
+        Hyperedges needsInterface(const Hyperedges& algorithmIds, const Hyperedges& inputIds);
+        // I/O & Dependencies
+        // RULE: I dependsOn O -> I is-a Input, O is-a Output
+        Hyperedges dependsOn(const Hyperedges& inputIds, const Hyperedges& outputIds);
+};
+
+}
+
+#endif
---
+++ src/SoftwareGraph.cpp
@@ -0,0 +1,195 @@
+#include "SoftwareGraph.hpp"
+
+namespace Software {
+
+
+// Concept Ids
+const UniqueId Graph::InterfaceId       = "Software::Graph::Interface";
+const UniqueId Graph::InputId           = "Software::Graph::Input";
+const UniqueId Graph::OutputId          = "Software::Graph::Output";
+const UniqueId Graph::AlgorithmId       = "Software::Graph::Algorithm";
+const UniqueId Graph::ImplementationId  = "Software::Graph::Implementation";
+const UniqueId Graph::DatatypeId        = "Software::Graph::Datatype";
+// Relation Ids
+const UniqueId Graph::DependsOnId       = "Software::Graph::DependsOn";
+const UniqueId Graph::NeedsId           = "Software::Graph::Needs";
+const UniqueId Graph::ProvidesId        = "Software::Graph::Provides";
+
+// GRAPH STUFF
+void Graph::createMainConcepts()
+{
+    createComponent(Graph::AlgorithmId, "ALGORITHM"); // Abstract, semantic definition (e.g. Localize)
+    Component::Network::createInterface(Graph::InterfaceId, "INTERFACE"); // Abstract, semantic interface (e.g. Position)
+    Component::Network::createInterface(Graph::InputId, "INPUT", Hyperedges{Graph::InterfaceId});
+    Component::Network::createInterface(Graph::OutputId, "OUTPUT", Hyperedges{Graph::InterfaceId});
+    createComponent(Graph::ImplementationId, "IMPLEMENTATION", Hyperedges{Graph::AlgorithmId}); // Concrete algorithm (e.g. 3DSLAM.cpp)
+    Component::Network::createInterface(Graph::DatatypeId, "DATATYPE", Hyperedges{Graph::InterfaceId}); // Concrete interface (e.g. Eigen::Vector3f)
+    // TODO: Where do we put language? Would it be better to encode different languages using domains?
+    // We could also just subclass implementation into implementation::C++ and implementation::VHDL
+
+    // Relations
+    // TODO: Specifying new subrelations should be easier. Put a convenience function into CommonConceptGraph class!
+    relate(Graph::DependsOnId, Hyperedges{Graph::InputId}, Hyperedges{Graph::OutputId}, "DEPENDS-ON");
+    subrelationOf(Graph::DependsOnId, CommonConceptGraph::ConnectsId);
+
+    relate(Graph::NeedsId, Hyperedges{Graph::AlgorithmId}, Hyperedges{Graph::InputId}, "NEEDS");
+    subrelationOf(Graph::NeedsId, CommonConceptGraph::HasAId);
+
+    relate(Graph::ProvidesId, Hyperedges{Graph::AlgorithmId}, Hyperedges{Graph::OutputId}, "PROVIDES");
+    subrelationOf(Graph::ProvidesId, CommonConceptGraph::HasAId);
+}
+
+Graph::Graph()
+{
+    createMainConcepts();
+}
+
+Graph::Graph(Component::Network& A)
+: Component::Network(A)
+{
+    createMainConcepts();
+}
+
+Graph::~Graph()
+{
+}
+
+Hyperedges Graph::createAlgorithm(const UniqueId& uid, const std::string& name, const Hyperedges& suids)
+{
+    return createComponent(uid, name, suids.empty() ? Hyperedges{Graph::AlgorithmId} : intersect(algorithmClasses(), suids));
+}
+
+Hyperedges Graph::createInput(const UniqueId& uid, const std::string& name, const Hyperedges& suids)
+{
+    return createInterface(uid, name, suids.empty() ? Hyperedges{Graph::InputId} : intersect(inputClasses(), suids));
+}
+
+Hyperedges Graph::createOutput(const UniqueId& uid, const std::string& name, const Hyperedges& suids)
+{
+    return createInterface(uid, name, suids.empty() ? Hyperedges{Graph::OutputId} : intersect(outputClasses(), suids));
+}
+
+Hyperedges Graph::createInterface(const UniqueId& uid, const std::string& name, const Hyperedges& suids)
+{
+    return Component::Network::createInterface(uid, name, suids.empty() ? Hyperedges{Graph::InterfaceId} : intersect(interfaceClasses(), suids));
+}
+
+Hyperedges Graph::createImplementation(const UniqueId& uid, const std::string& name, const Hyperedges& suids)
+{
+    return createAlgorithm(uid, name, suids.empty() ? Hyperedges{Graph::ImplementationId} : intersect(implementationClasses(), suids));
+}
+
+Hyperedges Graph::createDatatype(const UniqueId& uid, const std::string& name, const Hyperedges& suids)
+{
+    return createInterface(uid, name, suids.empty() ? Hyperedges{Graph::DatatypeId} : intersect(datatypeClasses(), suids));
+}
+
+Hyperedges Graph::algorithmClasses(const std::string& name, const Hyperedges& suids)
+{
+    Hyperedges all(componentClasses(name, Hyperedges{Graph::AlgorithmId}));
+    return suids.empty() ? all : intersect(all, subclassesOf(suids, name));
+}
+
+Hyperedges Graph::interfaceClasses(const std::string& name, const Hyperedges& suids)
+{
+    Hyperedges all(Component::Network::interfaceClasses(name, Hyperedges{Graph::InterfaceId}));
+    return suids.empty() ? all : intersect(all, subclassesOf(suids, name));
+}
+
+Hyperedges Graph::inputClasses(const std::string& name, const Hyperedges& suids)
+{
+    Hyperedges all(interfaceClasses(name, Hyperedges{Graph::InputId}));
+    return suids.empty() ? all : intersect(all, subclassesOf(suids, name));
+}
+
+Hyperedges Graph::outputClasses(const std::string& name, const Hyperedges& suids)
+{
+    Hyperedges all(interfaceClasses(name, Hyperedges{Graph::OutputId}));
+    return suids.empty() ? all : intersect(all, subclassesOf(suids, name));
+}
+
+Hyperedges Graph::implementationClasses(const std::string& name, const Hyperedges& suids)
+{
+    Hyperedges all(algorithmClasses(name, Hyperedges{Graph::ImplementationId}));
+    return suids.empty() ? all : intersect(all, subclassesOf(suids, name));
+}
+
+Hyperedges Graph::datatypeClasses(const std::string& name, const Hyperedges& suids)
+{
+    Hyperedges all(interfaceClasses(name, Hyperedges{Graph::DatatypeId}));
+    return suids.empty() ? all : intersect(all, subclassesOf(suids, name));
+}
+
+Hyperedges Graph::algorithms(const std::string& name, const std::string& className)
+{
+    // Get all super classes
+    Hyperedges classIds = algorithmClasses(className);
+    // ... and then the instances of them
+    return instancesOf(classIds, name);
+}
+Hyperedges Graph::interfaces(const std::string& name, const std::string& className)
+{
+    // Get all super classes
+    Hyperedges classIds = interfaceClasses(className);
+    // ... and then the instances of them
+    return instancesOf(classIds, name);
+}
+Hyperedges Graph::inputs(const std::string& name, const std::string& className)
+{
+    // Get all super classes
+    Hyperedges classIds = inputClasses(className);
+    // ... and then the instances of them
+    return instancesOf(classIds, name);
+}
+Hyperedges Graph::outputs(const std::string& name, const std::string& className)
+{
+    // Get all super classes
+    Hyperedges classIds = outputClasses(className);
+    // ... and then the instances of them
+    return instancesOf(classIds, name);
+}
+Hyperedges Graph::implementations(const std::string& name, const std::string& className)
+{
+    // Get all super classes
+    Hyperedges classIds = implementationClasses(className);
+    // ... and then the instances of them
+    return instancesOf(classIds, name);
+}
+Hyperedges Graph::datatypes(const std::string& name, const std::string& className)
+{
+    // Get all super classes
+    Hyperedges classIds = datatypeClasses(className);
+    // ... and then the instances of them
+    return instancesOf(classIds, name);
+}
+Hyperedges Graph::providesInterface(const Hyperedges& algorithmIds, const Hyperedges& outputIds)
+{
+    // An algorithm class or instance can only have an output instance
+    Hyperedges fromIds = unite(intersect(this->algorithms(), algorithmIds), intersect(algorithmClasses(), algorithmIds));
+    Hyperedges toIds = intersect(this->outputs(), outputIds);
+    if (fromIds.size() && toIds.size())
+        return relateFrom(fromIds, toIds, Graph::ProvidesId);
+    return Hyperedges();
+}
+
+Hyperedges Graph::needsInterface(const Hyperedges& algorithmIds, const Hyperedges& inputIds)
+{
+    // An algorithm class or instance can only have an output instance
+    Hyperedges fromIds = unite(intersect(this->algorithms(), algorithmIds), intersect(algorithmClasses(), algorithmIds));
+    Hyperedges toIds = intersect(this->inputs(), inputIds);
+    if (fromIds.size() && toIds.size())
+        return relateFrom(fromIds, toIds, Graph::NeedsId);
+    return Hyperedges();
+}
+
+Hyperedges Graph::dependsOn(const Hyperedges& inputIds, const Hyperedges& outputIds)
+{
+    // For now only input instances can depend on output instances
+    Hyperedges fromIds = intersect(this->inputs(), inputIds);
+    Hyperedges toIds = intersect(this->outputs(), outputIds);
+    if (fromIds.size() && toIds.size())
+        return relateFrom(fromIds, toIds, Graph::DependsOnId);
+    return Hyperedges();
+}
+
+}
